08b84ee27e07d2ae421ffb53fba6a0300a6fb23f
JIE <1765659336@qq.com>
2022-08-28 14:59:31 +0800

    [ADD]新增附件上传组件

---
+++ src/components/UploadFile/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="upload-file-container">
+    <div class="col-10 file-container">
+      <div v-for="item in 20" :key="item" class="file">
+        {{ item }}
+      </div>
+    </div>
+    <div class="col-2 upload-btn">
+      <el-upload
+        class="avatar-uploader"
+        action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+        :show-file-list="false"
+        :on-success="handleAvatarSuccess"
+        :before-upload="beforeAvatarUpload"
+      >
+        <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+        <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+      </el-upload>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { ElMessage } from "element-plus";
+import { Plus } from "@element-plus/icons-vue";
+
+import type { UploadProps } from "element-plus";
+
+const imageUrl = ref("");
+
+const handleAvatarSuccess: UploadProps["onSuccess"] = (
+  response,
+  uploadFile
+) => {
+  imageUrl.value = URL.createObjectURL(uploadFile.raw!);
+};
+
+const beforeAvatarUpload: UploadProps["beforeUpload"] = (rawFile) => {
+  if (rawFile.type !== "image/jpeg") {
+    ElMessage.error("Avatar picture must be JPG format!");
+    return false;
+  } else if (rawFile.size / 1024 / 1024 > 2) {
+    ElMessage.error("Avatar picture size can not exceed 2MB!");
+    return false;
+  }
+  return true;
+};
+</script>
+
+<style scoped lang="less">
+@import url("./index.less");
+</style>
