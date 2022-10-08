db108deb225c20d7b0e897a1d0c96c38c083188e
Liu Jie <1765659336@qq.com>
2022-10-08 20:59:44 +0800

    [UPDATE]自定义签名新增回退，清空，下载功能

--- src/components/CustomComponents/CustomSignature/hooks/useBrush.ts
+++ src/components/CustomComponents/CustomSignature/hooks/useBrush.ts
@@ -1,14 +1,22 @@
-import { Ref } from "vue";
+import { reactive, Ref } from "vue";
 
-export default function useBrush(canvasRef: Ref) {
+export default function useBrush(
+  canvasRef: Ref<HTMLElement>,
+  width: string,
+  height: string
+) {
   let isDrawing = false;
-  const ctx = canvasRef.getContext("2d");
-  canvasRef.addEventListener("mousedown", (e) => {
+  const ctx = canvasRef.value.getContext("2d");
+  let cacheData: Array<any> = [];
+  canvasRef.value.addEventListener("mousedown", (e) => {
     isDrawing = true;
     // 给canvas绘制定位
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "red";
     ctx.moveTo(e.pageX, e.pageY);
   });
-  canvasRef.addEventListener("mousemove", (e) => {
+  canvasRef.value.addEventListener("mousemove", (e) => {
     if (isDrawing) {
       // 绘制路径
       ctx.lineTo(e.pageX, e.pageY);
@@ -16,7 +24,37 @@ export default function useBrush(canvasRef: Ref) {
       ctx.stroke();
     }
   });
-  canvasRef.addEventListener("mouseup", (e) => {
+  canvasRef.value.addEventListener("mouseup", (e) => {
     isDrawing = false;
+    cacheData.push(ctx.getImageData(0, 0, width, height));
+    console.log(cacheData);
   });
+
+  // 清空
+  const clear = function () {
+    ctx.clearRect(0, 0, width, height);
+  };
+
+  // 下载
+  const downLoad = function () {
+    canvasRef.value.toBlob((blob) => {
+      const a = document.createElement("a");
+      document.body.append(a);
+      a.download = `签名.png`;
+      a.href = URL.createObjectURL(blob);
+      a.click();
+      a.remove();
+    });
+  };
+
+  // 回退
+  const fallback = function () {
+    ctx.putImageData(cacheData.pop(), 0, 0);
+  };
+
+  return {
+    clear,
+    downLoad,
+    fallback,
+  };
 }
--- src/components/CustomComponents/CustomSignature/index.vue
+++ src/components/CustomComponents/CustomSignature/index.vue
@@ -1,16 +1,28 @@
 <template>
   <div class="custom-signature-container">
-    <canvas ref="cvs" width="500" height="300"></canvas>
+    <el-button type="primary" @click="clearCopy">清空</el-button>
+    <el-button type="primary" @click="fallbackCopy">回退</el-button>
+    <el-button type="primary" @click="downLoadCopy">下载</el-button>
+    <el-button type="primary">保存</el-button>
+    <canvas ref="cvs" :width="width" :height="height"></canvas>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from "vue";
+import { onMounted, reactive, ref } from "vue";
 import useBrush from "./hooks/useBrush";
 // canvas绘制主体逻辑(下笔收笔)
 const cvs = ref();
-watch(cvs, function () {
-  useBrush(cvs.value);
+const width = "1000";
+const height = "500";
+const clearCopy = ref();
+const downLoadCopy = ref();
+const fallbackCopy = ref();
+onMounted(() => {
+  const { clear, downLoad, fallback } = useBrush(cvs, width, height);
+  clearCopy.value = clear;
+  downLoadCopy.value = downLoad;
+  fallbackCopy.value = fallback;
 });
 </script>
 
