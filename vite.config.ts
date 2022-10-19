0141d929000353e51b06e5a4f63eef6d32545afc
Liu Jie <1765659336@qq.com>
2022-10-19 17:21:44 +0800

    [ADD]引入defineOptions插件给组件命名便于vue开发者工具的调试

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 import { viteMockServe } from "vite-plugin-mock";
+import DefineOptions from 'unplugin-vue-define-options/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
       supportTs: true,
       mockPath: "./src/mock/",
     }),
+    DefineOptions()
   ],
   // 别名
   resolve: {
