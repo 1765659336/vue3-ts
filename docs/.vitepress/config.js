a32971152f5a7b4b3df026c1e60d41c6a20d9296
Liu Jie <1765659336@qq.com>
2022-10-28 23:17:05 +0800

    [ADD]为项目添加vitePress,设置初始版本侧边栏

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,43 @@
+export default {
+    title: 'Vue3-Ts',
+    description: '',
+    themeConfig: {
+        sidebar: [
+            {
+                text: 'Custom自定义',
+                items: [
+                    { text: 'Affix固钉', link: '/Custom/Affix' },
+                    { text: 'Aside布局-侧边', link: '/Custom/Aside' },
+                    { text: 'BackTop回到顶部', link: '/Custom/BackTop' },
+                    { text: 'Badge徽章', link: '/Custom/Badge' },
+                    { text: 'Button按钮', link: '/Custom/Button' },
+                    { text: 'Cantainer布局容器', link: '/Custom/Cantainer' },
+                    { text: 'Checkbox复选框', link: '/Custom/Checkbox' },
+                    { text: 'Collapse折叠面板', link: '/Custom/Collapse' },
+                    { text: 'CollapseItem折叠面板子元素', link: '/Custom/CollapseItem' },
+                    { text: 'Dropdown下拉菜单', link: '/Custom/Dropdown' },
+                    { text: 'DropdownItem下拉菜单子元素', link: '/Custom/DropdownItem' },
+                    { text: 'Footer布局-底部', link: '/Custom/Footer' },
+                    { text: 'Header布局-头部', link: '/Custom/Header' },
+                    { text: 'Link文字链接', link: '/Custom/Link' },
+                    { text: 'Main布局-主体', link: '/Custom/Main' },
+                    { text: 'Message消息提示框', link: '/Custom/Message' },
+                    { text: 'Popover气泡弹出框', link: '/Custom/Popover' },
+                    { text: 'Rate评分', link: '/Custom/Rate' },
+                    { text: 'Signature绘画板', link: '/Custom/Signature' },
+                    { text: 'Slider滑块', link: '/Custom/Slider' },
+                    { text: 'Switch开关', link: '/Custom/Switch' },
+                    { text: 'Tag标签', link: '/Custom/Tag' },
+                    { text: 'Timeline时间轴', link: '/Custom/Timeline' },
+                    { text: 'Transfer穿梭框', link: '/Custom/Transfer' },
+                ]
+            },
+            {
+                text: 'SecondaryPackaging二次封装',
+                items: [
+                    { text: 'SecondEchart', link: '/SecondaryPackaging/SecondEchart' },
+                ]
+            }
+        ]
+    },
+}
